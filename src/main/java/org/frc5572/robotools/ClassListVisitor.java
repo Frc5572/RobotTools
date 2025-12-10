1bf854f7e1acb531fc9f3b45c0216c0b44ba9f72
wilsonwatson <wilsonwatson@utexas.edu>
2025-12-10 17:28:25 -0600

    generate empty io

---
+++ src/main/java/org/frc5572/robotools/ClassListVisitor.java
@@ -0,0 +1,32 @@
+package org.frc5572.robotools;
+
+import java.util.List;
+
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.SimpleAnnotationValueVisitor8;
+
+public class ClassListVisitor extends SimpleAnnotationValueVisitor8<Void, Void> {
+    
+    private final List<TypeMirror> mirrors;
+
+    public ClassListVisitor(List<TypeMirror> mirrors) {
+        super();
+        this.mirrors = mirrors;
+    }
+
+    @Override
+    public Void visitType(TypeMirror arg0, Void arg1) {
+        mirrors.add(arg0);
+        return null;
+    }
+
+    @Override
+    public Void visitArray(List<? extends AnnotationValue> arg0, Void arg1) {
+        for(var item : arg0) {
+            this.visit(item);
+        }
+        return null;
+    }
+
+}
